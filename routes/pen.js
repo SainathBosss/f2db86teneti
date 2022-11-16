2816e882c9b6a6dfcf0ec3bed0d2758bef2250fa
SainathBosss <sainathreddyteneti@gmail.com>
2022-11-15 21:53:08 -0600

    changes done

--- routes/pen.js
+++ routes/pen.js
@@ -3,9 +3,11 @@ const { pen_view_one_Page } = require('../controllers/pen');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('pens', { title: 'Search Results'});
 });
+*/
+router.get('/', pen.pen_view_all_page);
 router.get('/detail', pen.pen_view_one_Page);
 
 module.exports = router;
